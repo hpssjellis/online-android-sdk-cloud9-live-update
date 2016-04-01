3bd78b8edc1c832e06f262e4e2dc56d2d60cdbd3
Jeremy Ellis <keyfreemusic@gmail.com>
2016-03-31 23:10:09 -0700

    Create a10-generate-ant-release.sh

---
+++ ubuntu/start-here/a10-generate-ant-release.sh
@@ -0,0 +1,7 @@
+#!/bin/bash  
+
+# Find or copy this file to your cloud9 Android-SDK workspace
+# run it with the command
+# bash a10-generate-ant-release.sh
+
+# not the best way. Try another approach

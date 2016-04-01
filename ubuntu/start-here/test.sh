b2cf7a2ad88de661a6bb84b80327e31d76e2b5bd
Jeremy Ellis <keyfreemusic@gmail.com>
2016-03-31 18:18:22 -0700

    Update test.sh

--- ubuntu/start-here/test.sh
+++ ubuntu/start-here/test.sh
@@ -1,1 +1,14 @@
-test
+#!/bin/bash  
+
+#  only need to run this script with the command (do not type the #)
+#  bash a04-restart-webserver.sh
+#  best in cloud 9 to just right click this file and select run
+
+
+
+
+
+echo "Activating apache2 webserver"
+
+service apache2 start
+echo "cool"

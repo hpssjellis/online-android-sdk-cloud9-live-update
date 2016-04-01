163ebb994c7f9584cd04571896213d38ab94313a
Jeremy Ellis <keyfreemusic@gmail.com>
2016-03-31 23:31:38 -0700

    Update and rename a10-generate-ant-release.sh to a10-generate-gradle-release.sh

--- ubuntu/start-here/a10-generate-gradle-release.sh
+++ ubuntu/start-here/a10-generate-gradle-release.sh
@@ -2,6 +2,6 @@
 
 # Find or copy this file to your cloud9 Android-SDK workspace
 # run it with the command
-# bash a10-generate-ant-release.sh
+# bash a10-generate-gradle-release.sh
 
 # not the best way. Try another approach
